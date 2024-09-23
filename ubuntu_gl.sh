c7e47877eec6e4de8901f798fde7b03567a12ea3
Tri Pham <techcaotri@gmail.com>
2024-09-23 12:01:15 +0700

    Run uniclip for clipboard sharing

--- ubuntu_gl.sh
+++ ubuntu_gl.sh
@@ -21,16 +21,56 @@ WHITE='\033[1;37m'
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
+run_uniclip() {
+	# Kill all uniclip processes
+	killall uniclip
+
+	# Define the log file path
+	LOG_FILE="/tmp/uniclip_output.log"
+
+	# Run uniclip in the background and redirect output to the log file
+	uniclip -d -p 5100 >"$LOG_FILE" 2>&1 &
+
+	# Capture the process ID
+	UNICLIP_PID=$!
+
+	echo "uniclip server is running in the background with PID $UNICLIP_PID"
+
+	# Function to extract IP:Port from the log file
+	extract_ip_port() {
+		grep "Run \`uniclip" "$LOG_FILE" | awk -F'`' '{print $2}' | awk '{print $2}'
+	}
+
+	# Wait for the IP:Port information to appear in the log file
+	echo "Waiting for IP:Port information..."
+	for i in {1..30}; do
+		IP_PORT=$(extract_ip_port)
+		if [ -n "$IP_PORT" ]; then
+			echo "Extracted IP:Port is $IP_PORT"
+			uniclip "$IP_PORT" &
+			break
+		fi
+		sleep 1
+	done
+
+	# Check if we successfully extracted the IP:Port
+	if [ -z "$IP_PORT" ]; then
+		echo "Failed to extract IP:Port within 30 seconds. Check the log file at $LOG_FILE"
+	fi
+
+	echo "uniclip server is still running in the background. To stop it, use: kill $UNICLIP_PID"
+}
+
 BOOT_BIN=/usr/bin/qemu-system-x86_64
 NETNAME=ubuntu
 # MAC=$(grep -e "${NETNAME}=" macs.txt |cut -d"=" -f 2)
 HOSTNAME=${NETNAME}
-MEM=12G
+MEM=24G
 DP=sdl,gl=on
 MTYPE=pc-q35-6.2,accel=kvm,dump-guest-core=off,mem-merge=on,smm=on,vmport=off,nvdimm=off,hmat=on,memory-backend=mem1
 ACCEL=accel=kvm,kvm-shadow-mem=256000000,kernel_irqchip=on
 UUID="$(uuidgen)"
-CPU=8,sockets=4,cores=2,threads=1
+CPU=24,sockets=6,cores=4,threads=1
 BIOS=${SCRIPT_DIR}/kde-neon_VARS.fd
 # BIOS=/usr/share/OVMF/OVMF_VARS_4M.ms.fd
 ISODIR=/applications/OS/isos
@@ -78,7 +118,7 @@ args=(
 	# -device hda-micro,audiodev=hda
 	-audiodev pa,id=snd0,server=localhost -device AC97,audiodev=snd0
 	-device virtio-net,netdev=nic
-	-netdev user,hostname=kdeneon-user,hostfwd=tcp::22220-:22,hostfwd=tcp::5100-:5100,id=nic
+	-netdev user,hostname=kdeneon-user,hostfwd=tcp::22220-:22,id=nic
 	-chardev pty,id=charserial0
 	-device isa-serial,chardev=charserial0,id=serial0
 	-device virtio-serial
@@ -112,6 +152,8 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 echo -e "${LIGHTBLUE}Start VirtioFS Daemon virtiofsd for sharing Downloads directory ...${NOCOLOR}"
 sudo rm -f /tmp/vhostqemu
 sudo /usr/lib/qemu/virtiofsd --socket-path=/tmp/vhostqemu --socket-group=tripham -o source=/home/tripham/Downloads/ -o allow_direct_io -o cache=always &
+
+run_uniclip
 
 echo ${BOOT_BIN} "${args[@]}"
 GTK_BACKEND=x11 GDK_BACKEND=x11 QT_BACKEND=x11 VDPAU_DRIVER="nvidia" ${BOOT_BIN} "${args[@]}"
